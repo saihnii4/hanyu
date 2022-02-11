4904b52645ec53683c67bd347ccb2f6c351197da
dulguuncodes <81787757+dulguuncodes@users.noreply.github.com>
2022-02-11 14:46:44 +0800

    init commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Here is an empty template cause why not</Text>
       <StatusBar style="auto" />
     </View>
   );
